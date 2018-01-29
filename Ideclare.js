a047201c14e6db572fac0197f3c1d0fa1d223ca8
Eric Pricop <dimitripricop@yahoo.com>
2018-01-29 10:37:03 -0800

    final changes

--- Ideclare.js
+++ Ideclare.js
@@ -1,23 +1,23 @@
 var myAge = 22;
-console.log("myAge"); 
+console.log(myAge); 
 var year = 2018; 
-console.log("year");
+console.log(year);
 var ParkingSpace = 206;
-console.log("ParkingSpace"); 
+console.log(ParkingSpace); 
 var phoneNum = 4255336911; 
-console.log("phoneNum"); 
+console.log(phoneNum); 
 
 var Laptop = 'MSI';
-console.log("Laptop");  
+console.log(Laptop);  
 var Script = 'Java'; 
-console.log("Script"); 
+console.log(Script); 
 var Goal = 'Graduate'; 
-console.log("Goal"); 
+console.log(Goal); 
 
 var theyLost = false; 
-console.log("theyLost"); 
+console.log(theyLost); 
 var PatsWin = true; 
-console.log("PatsWin"); 
+console.log(PatsWin); 
 
 var arr = []; 
-console.log("arr"); 
+console.log(arr); 
